8fbf4a0f39ea9d06358fc6bd422029a9dcd6b35e
ikki-sasa <manga.samadjine@3wa.io>
2021-01-28 18:10:31 +0100

    error-lg-09.js

--- exercices/Projet Paris - Base/controllers/SearchController.js
+++ exercices/Projet Paris - Base/controllers/SearchController.js
@@ -4,13 +4,17 @@ class SearchController {
 	}
 	// méthode déclenchée après que le DOM de la page principale ait été mis à jour avec les contenus de la vue partielle
 	executeAfterDomUpdate() {
-		// [TODO] surveiller l'évènement submit du formulaire
-        console.log("search ok");
-        document.querySelector('#searchForm').addEventListener("submit", (e) => {
-            e.preventDefault();
-            const q = document.querySelector("#q").value;
-            console.log(q);
-        });
+		console.log("search ok");
+		// surveiller l'évènement submit du formulaire
+		document.querySelector("#searchForm").addEventListener("submit", (e) => {
+			// empecher le submit d'avoir son comportement normal (cad envoyer une requête HTTP)
+			e.preventDefault();
+			// récupérer les valeurs saisies dans le formulaire
+			const q = document.querySelector("#q").value;
+			console.log(q);
+
+			// [TODO] Envoyer les valeurs de recherche à l'API open data Paris, qui nous renverra des évènements, qu'on ira afficher proprement dans la page
+		});
 	}
 }
 export default SearchController;
