1dbbfd3ae633aedbbd3873b86b3d85d424330a3c
ikki-sasa <manga.samadjine@3wa.io>
2021-01-29 12:34:58 +0100

    push

--- exercices/Projet Paris - Base/webpack.config.js
+++ exercices/Projet Paris - Base/webpack.config.js
@@ -1,9 +1,17 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
     entry: "./app/main.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Paris Events",
+            template: "index.html",
+        }),
+    ],
 };
